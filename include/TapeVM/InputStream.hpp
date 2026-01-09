3c4491c71d8c26c683306a6143f7fab21b296642
Sanguine Noctis <81781310+ImpishDeathTech@users.noreply.github.com>
2026-01-09 00:37:02 -0500

    Update InputStream.hpp

--- include/TapeVM/InputStream.hpp
+++ include/TapeVM/InputStream.hpp
@@ -1,13 +1,13 @@
-/* InputStream.hpp
+/* TapeVM/InputStream.hpp
  * Copyright (c) 2020-2025, Christopher Stephen Rafuse
  * BSD-2-Clause
  */
 #pragma once
 
-#include <NoctSys/Scripting/TapeVM/Configuration.hxx>
-#include <NoctSys/Scripting/TapeVM/InputStream/InputSource.hpp>
-#include <NoctSys/Scripting/TapeVM/InputStream/StringInputSource.hpp>
-#include <NoctSys/Scripting/TapeVM/InputStream/FileInputSource.hpp>
+#include <TapeVM/Configuration.hxx>
+#include <TapeVM/InputStream/InputSource.hpp>
+#include <TapeVM/InputStream/StringInputSource.hpp>
+#include <TapeVM/InputStream/FileInputSource.hpp>
 
 #include <vector>
 #include <memory>
@@ -25,4 +25,5 @@ namespace noct {
     int  get();
     void unget();
   };
+
 }
