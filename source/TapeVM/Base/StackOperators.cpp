a2a04e9b2730eacb92c7eb52acb23f8bfdc5e6ac
Sanguine Noctis <81781310+ImpishDeathTech@users.noreply.github.com>
2026-01-09 01:18:01 -0500

    Update StackOperators.cpp

--- source/TapeVM/Base/StackOperators.cpp
+++ source/TapeVM/Base/StackOperators.cpp
@@ -1,9 +1,9 @@
 /* TapeVM/Base/StackOperators.cpp
- * Copyright (c) 2020-2025, Christopher Stephen Rafuse
+ * Copyright (c) 2026, Christopher Stephen Rafuse
  * BSD-2-Clause
  */
-#include <NoctSys/Scripting/TapeVM.hpp>
-#include <NoctSys/Exception/TapeError.hpp>
+#include <TapeVM.hpp>
+#include <TapeVM/TapeError.hpp>
 
 #include <cassert>
 #include <cmath>
@@ -287,4 +287,5 @@ namespace noct {
       else throw TapeError("Stack Underflow", "R>");
     });
   }
+
 }
