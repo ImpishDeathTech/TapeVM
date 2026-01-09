2ca88c319cc04419b0cd80a0491d36f455873e72
Sanguine Noctis <81781310+ImpishDeathTech@users.noreply.github.com>
2026-01-09 00:43:27 -0500

    Update FileOutputSource.hpp

--- include/TapeVM/OutputStream/FileOutputSource.hpp
+++ include/TapeVM/OutputStream/FileOutputSource.hpp
@@ -1,15 +1,15 @@
-/* FileOutputSource.hpp
+/* TapeVM/OutputStream/FileOutputSource.hpp
  * Copyright (c) 2020-2025, Christopher Stephen Rafuse
  * BSD-2-Clause
  */
 #pragma once 
 
-#include <NoctSys/Configuration.hxx>
-#include <NoctSys/Scripting/TapeVM/OutputStream/OutputSource.hpp>
+#include <TapeVM/Configuration.hxx>
+#include <TapeVM/OutputStream/OutputSource.hpp>
 #include <cstdio>
 
 namespace noct {
-  class NoctSysAPI FileOutputSource 
+  class TapeAPI FileOutputSource 
     : public OutputSource<char>
   {
     std::FILE* m_fd;
@@ -29,4 +29,5 @@ namespace noct {
     void put(char ch)                              override;
     void flush()                                   override;
   };
+
 }
